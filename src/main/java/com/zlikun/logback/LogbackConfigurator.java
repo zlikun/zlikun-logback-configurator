349f8f871c6f8c751cea69353d92fa5f4b09522f
zlikun <zlikun-dev@hotmail.com>
2017-07-11 20:00:03 +0800

    配置RollingFileAppender

--- src/main/java/com/zlikun/logback/LogbackConfigurator.java
+++ src/main/java/com/zlikun/logback/LogbackConfigurator.java
@@ -12,6 +12,10 @@ import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.FileAppender;
 import ch.qos.logback.core.encoder.Encoder;
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.RollingPolicy;
+import ch.qos.logback.core.rolling.TimeBasedFileNamingAndTriggeringPolicy;
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.core.spi.ContextAwareBase;
 import ch.qos.logback.core.util.FileSize;
 
@@ -35,7 +39,9 @@ public class LogbackConfigurator extends ContextAwareBase implements Configurato
         ConsoleAppender<ILoggingEvent> consoleAppender = consoleAppender(encoder) ;
 
         // 文件Appender
-        FileAppender<ILoggingEvent> fileAppender = fileAppender(encoder) ;
+        // FileAppender<ILoggingEvent> fileAppender = fileAppender(encoder) ;
+        // 滚动文件Appender
+        FileAppender<ILoggingEvent> fileAppender = rollingFileAppender(encoder) ;
 
         // 设置ROOT输出配置[CONSOLE ,DEBUG ,false]
         setAppender(context.getLogger(Logger.ROOT_LOGGER_NAME), consoleAppender, Level.INFO, false);
@@ -45,11 +51,57 @@ public class LogbackConfigurator extends ContextAwareBase implements Configurato
     }
 
     /**
+     * 滚动文件Appender，自定义Encoder
+     * @param encoder
+     * @return
+     */
+    FileAppender<ILoggingEvent> rollingFileAppender(Encoder encoder) {
+        if (encoder == null) throw new IllegalArgumentException("encoder is required .") ;
+        RollingFileAppender<ILoggingEvent> appender = new RollingFileAppender<ILoggingEvent>();
+        appender.setContext(this.context);
+        appender.setName("file");
+
+        // 设置输出日志文件
+        appender.setAppend(true);
+        appender.setBufferSize(FileSize.valueOf("4kb"));
+        appender.setFile("./target/log/server.log");
+        appender.setPrudent(false);
+        appender.setImmediateFlush(true);
+
+        // 设置滚动策略
+        TimeBasedRollingPolicy policy = new TimeBasedRollingPolicy() ;
+        // 设置上下文
+        policy.setContext(this.context);
+        // 设置启动时清理历史
+        policy.setCleanHistoryOnStart(false);
+        // 设置最大历史文件5个
+        policy.setMaxHistory(5);
+        // 设置文件名称模板，这里用于测试，以秒滚动
+        policy.setFileNamePattern("./target/log/server.%d{HHmmss}.zip");
+        // 设置总文件容量，默认：0，表示不限制
+        policy.setTotalSizeCap(FileSize.valueOf("16kb"));
+        // parent and context required
+        // https://github.com/tony19/logback-android/wiki/Appender-Notes#configuration-in-code
+        policy.setParent(appender);
+        // 启动滚动策略
+        policy.start();
+        appender.setRollingPolicy(policy);
+
+        encoder.setContext(this.context);
+        if (!encoder.isStarted()) encoder.start();
+        appender.setEncoder(encoder);
+
+        appender.start();
+        return appender ;
+    }
+
+    /**
      * 文件Appender，自定义Encoder
      * @param encoder
      * @return
      */
     FileAppender<ILoggingEvent> fileAppender(Encoder encoder) {
+        if (encoder == null) throw new IllegalArgumentException("encoder is required .") ;
         FileAppender<ILoggingEvent> appender = new FileAppender<ILoggingEvent>() ;
         // 设置context要在设置文件相关属性之前
         appender.setContext(this.context);
